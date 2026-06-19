7abe5812a4e5be4c76d9497e18bdeb5b07091a1f
angrypig555 <brny@brny.dev>
2026-06-19 21:43:42 +0100

    add server

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,3 +1,8 @@
-fn main() {
+use std::io::prelude::*;
+use std::net::TcpListener;
+
+fn main() -> std::io::Result<()> {
     println!("server");
+    let listener = TcpListener::bind("0.0.0.0:6434")?;
+    Ok(())
 }
