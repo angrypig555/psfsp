215646ec5e3f327afb63276f4523660036ae77fd
angrypig555 <brny@brny.dev>
2026-06-20 09:08:05 +0100

    add the port binding

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,3 +1,16 @@
-fn main() {
+use std::io::prelude::*;
+use std::net::TcpStream;
+
+use psfsp::GREET;
+
+fn main() -> std::io::Result<()> {
     println!("client");
+    let mut stream = TcpStream::connect("127.0.0.1:6434")?;
+    stream.write_all(&[GREET])?;
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer)?;
+    let bytes = [buffer[1], buffer[2]];
+    let redirected_port = u16::from_be_bytes(bytes);
+    println!("new port is {}", redirected_port);
+    Ok(())
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,8 +1,36 @@
 use std::io::prelude::*;
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+
+use psfsp::{GREET, REDIRECTED};
+
+fn handle_client(first_stream: &mut TcpStream) -> std::io::Result<()> {
+    let listener = TcpListener::bind("0.0.0.0:0")?;
+    let new_port = listener.local_addr()?.port();
+    println!("got new temp port {}", new_port);
+    let new_port_u8 = new_port.to_be_bytes();
+    let redirection = [REDIRECTED, new_port_u8[0], new_port_u8[1]];
+    first_stream.write_all(&redirection)?;
+    Ok(())
+}
 
 fn main() -> std::io::Result<()> {
     println!("server");
     let listener = TcpListener::bind("0.0.0.0:6434")?;
+    for stream in listener.incoming() {
+        println!("got connection");
+        let mut stream = stream?;
+        let mut buffer = [0; 128];
+        let greeting = stream.read(&mut buffer)?;
+        if greeting <= 0 {
+            println!("client sent no data");
+            continue;
+        }
+        let greet_byte = buffer[0];
+        if greet_byte != GREET {
+            println!("client sent incorrect magic byte");
+            continue;
+        }
+        handle_client(&mut stream)?;
+    }
     Ok(())
 }
